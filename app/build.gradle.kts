aa2c2e039189cb983393d28f8595edd2f3c7d1f9
lvtong199881 <you@example.com>
2026-04-27 01:09:21 +0800

    fix: 修复错误依赖

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -173,17 +173,17 @@ dependencies {
     // ========== 基础组件 (Maven) ==========
     implementation("com.mohanlv:base:1.2.27")
     implementation("com.mohanlv:startup:1.2.24")
-    implementation("com.mohanlv:router:1.2.9router")
+    implementation("com.mohanlv:router:1.2.9")
     implementation("com.mohanlv:network:1.2.14")
     implementation("com.mohanlv:logger:1.2.21")
 
     // ========== 业务组件 (Maven) ==========
-    implementation("com.mohanlv:login:1.2.9loginlogin")
+    implementation("com.mohanlv:login:1.2.9")
     implementation("com.mohanlv:home:1.2.13")
-    implementation("com.mohanlv:user:1.2.11user")
-    implementation("com.mohanlv:reactnative:1.2.9reactnativereactnative")
-    implementation("com.mohanlv:websdk:1.2.14websdk")
-    implementation("com.mohanlv:shortvideo:1.2.15shortvideoshortvideo")
+    implementation("com.mohanlv:user:1.2.11")
+    implementation("com.mohanlv:reactnative:1.2.9")
+    implementation("com.mohanlv:websdk:1.2.14")
+    implementation("com.mohanlv:shortvideo:1.2.15")
     // ===================================
     
     // Lifecycle
